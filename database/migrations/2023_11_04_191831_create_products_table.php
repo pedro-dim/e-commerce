9792b25e118cd73ea50a021c6f679071861cccf3
pedro-dim <pcontato19@gmail.com>
2023-11-04 19:34:18 +0000

    creating database migration

---
+++ database/migrations/2023_11_04_191831_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->integer('quantity');
+            $table->string('sku')->unique();
+            $table->foreignIdFor(Category::class);
+            $table->timestamps();
+            $table->softDeletes();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
